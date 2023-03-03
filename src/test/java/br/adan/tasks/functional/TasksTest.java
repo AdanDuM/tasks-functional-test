76ed0c39a6693e371dbab8652d8b98c6fd53605b
Adan Gomes <adandum@hotmail.com>
2023-03-03 01:54:48 -0300

    paralelismo em docker

--- src/test/java/br/adan/tasks/functional/TasksTest.java
+++ src/test/java/br/adan/tasks/functional/TasksTest.java
@@ -1,20 +1,23 @@
 package br.adan.tasks.functional;
 
 import static org.junit.Assert.assertEquals;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 
   private WebDriver driver;
 
   @Before
-  public void setup() {
+  public void setup() throws MalformedURLException {
     driver = acessarAplicacao();
   }
 
@@ -97,11 +100,14 @@ public class TasksTest {
     assertEquals("Fill the task description", mensagem);
   }
 
-  public WebDriver acessarAplicacao() {
-    System.setProperty("webdriver.chrome.driver",
-        "C:\\Users\\Adan\\Documents\\desenvolvimento\\libs\\driver\\browser\\chrome\\chromedriver.exe");
-    final WebDriver driver = new ChromeDriver();
-    driver.navigate().to("http://localhost:8001/tasks");
+  public WebDriver acessarAplicacao() throws MalformedURLException {
+    // System.setProperty("webdriver.chrome.driver",
+    // "C:\\Users\\Adan\\Documents\\desenvolvimento\\libs\\driver\\browser\\chrome\\chromedriver.exe");
+    // final WebDriver driver = new ChromeDriver();
+    final DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+    final WebDriver driver =
+        new RemoteWebDriver(new URL("http://192.168.15.129:4444/wd/hub"), capabilities);
+    driver.navigate().to("http://192.168.15.129:8001/tasks");
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     return driver;
   }
